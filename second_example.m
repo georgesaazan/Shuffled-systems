f517d51d6240694b13a9fd6aaee2c8802083f2cb
georgesaazan <81086197+georgesaazan@users.noreply.github.com>
2022-05-02 12:09:34 +0200

    Update second_example.m

--- second_example.m
+++ second_example.m
@@ -11,7 +11,7 @@
 %in gamma_result.m with the plots.
 clear all;
 C = {'r','b','g'}; %define colors used in the figure
-n=3:1:3; %for 3,4 and 5 oscillators
+for n=3:1:5 %for 3,4 and 5 oscillators
 resultx=[]; %initialize the result every iteration on the number of oscillators
 for gamma=0:0.01:0.9 %we change the oscillators gain \gamma from 0 to 0.9
     if(gamma<=0.8) %we verify that for \gamma<=0.8 the jsr is 1.02 for all n
@@ -34,7 +34,7 @@ ylabel("Inf -log(\rho)/log(\lambda)");
 title("stabilizability vs k");
 ylim([0 0.6]);
 hold on;
-
+end
 legend([a(1),a(2),a(3)],'3 oscillators','4 oscillators','5 oscillators');
 
 %% In the second part we give 2 simulations for the example
